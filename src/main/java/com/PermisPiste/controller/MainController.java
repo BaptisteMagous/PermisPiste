79316574bf9e8ff54255d1743bd8de3f57f96ea8
abc3354 <arnoniels@online.fr>
2021-06-14 18:37:39 +0200

    Peut envoyer vue mais pas avec le bon mime type

--- src/main/java/com/PermisPiste/controller/MainController.java
+++ src/main/java/com/PermisPiste/controller/MainController.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -42,7 +40,7 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public ModelAndView showIndex(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return new ModelAndView("index");
+    public String showIndex(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return "index";
     }
 }
